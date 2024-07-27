8bdc3f94a0c9044ea169b173227cdc0a16f09c86
teerenzo <renzahoemmanuel8@gmail.com>
2024-07-27 20:14:42 +0200

    connecting to mysql

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/test/todo.java
@@ -0,0 +1,8 @@
+package com.in28minutes.rest.webservices.restfulwebservices.test;
+
+public class todo {
+	
+	
+	
+
+}
---
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/TodoTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TodoTest {
+
+	@Test
+	void test() {
+//		fail("Not yet implemented");
+	}
+
+}
